b353430f4400c57c22dd148498d3589a7bc67a1d
John Kilima <jkbundala@gmail.com>
2024-05-06 13:52:28 +0900

    Setup project in IDE creating folder structure

---
+++ sports-shop-backend/src/main/java/dev/kilima/sports/SportsShopBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.kilima.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsShopBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsShopBackendApplication.class, args);
+	}
+
+}
---
+++ sports-shop-backend/src/test/java/dev/kilima/sports/SportsShopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kilima.sports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsShopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

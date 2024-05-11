04e193da242366b831640dd703772411a9987d5e
John Kilima <jkbundala@gmail.com>
2024-05-11 19:09:53 +0900

    Backend creating the Brand entity

---
+++ sports-shop-backend/src/main/java/dev/kilima/sports/entity/Brand.java
@@ -0,0 +1,28 @@
+package dev.kilima.sports.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="Brand")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Brand {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="Id")
+	private Integer id;
+	@Column(name="Name")
+	private String name;
+	
+}
